de4a5dc7ea6f8a6844f8c9077b9e47b46b587655
miranadhirah <amirah710@gmail.com>
2020-03-07 12:41:41 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Lab 3',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('FLUTTER TEST'),
+        ),
+        body: Container(
+            child: Center(
+              child: Wrap(
+                spacing: 8.0, // gap between adjacent chips
+                runSpacing: 4.0, // gap between lines
+                children: <Widget>[
+                  Column(
+                    children: <Widget>[
+                      Container(
+                        margin: const EdgeInsets.all(10.0),
+                        color: Colors.purple[600],
+                        width: 48.0,
+                        height: 48.0,
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: <Widget>[
+                      Container(
+                        margin: const EdgeInsets.all(10.0),
+                        color: Colors.blue[600],
+                        width: 48.0,
+                        height: 48.0,
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: <Widget>[
+                      Container(
+                        margin: const EdgeInsets.all(10.0),
+                        color: Colors.orange[600],
+                        width: 48.0,
+                        height: 48.0,
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: <Widget>[
+                      Container(
+                        margin: const EdgeInsets.all(10.0),
+                        color: Colors.amber[600],
+                        width: 48.0,
+                        height: 48.0,
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: <Widget>[
+                      Container(
+                        margin: const EdgeInsets.all(10.0),
+                        color: Colors.lime[600],
+                        width: 48.0,
+                        height: 48.0,
+                      ),
+                    ],
+                  ),
+                  Column(
+                    children: <Widget>[
+                      Container(
+                        margin: const EdgeInsets.all(10.0),
+                        color: Colors.green[600],
+                        width: 48.0,
+                        height: 48.0,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            )),
+
+        /*
+        body: Container(
+          child: Center(
+            child: Container(
+              margin: const EdgeInsets.all(10.0),
+              color: Colors.amber[600],
+              width: 48.0,
+              height: 48.0,
+            ),
+          )
+        ),
+        */
+      ),
+    );
+  }
+}
